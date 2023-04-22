83f806b9269db83b59be1a5d012decd9f1b4eea6
vladi <vladi@LAPTOP-MQML25RF>
2023-04-21 23:15:30 -0600

    Agregar archivos de proyecto.

---
+++ PracticaHelpers/Models/Context_Helper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PracticaHelpers.Models
+{
+    public class Context_Helper : DbContext
+    {
+        public Context_Helper(DbContextOptions options) : base(options){
+
+        }
+    }
+}
